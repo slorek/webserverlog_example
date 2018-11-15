3720e1df9e6b48111f5954c22e83a25e352f6291
Steve Lorek <steve@stevelorek.com>
2018-11-15 19:57:47 +0000

    First implementation of LogParser#most_visits

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,2 +1,23 @@
 class LogParser
+
+  attr_reader :log
+
+  def initialize(file_path)
+    raise ArgumentError.new("file_path must be a String") unless file_path.class == String
+
+    @log = []
+
+    IO.foreach(file_path) do |line|
+      @log << line.split(" ")
+    end
+  end
+
+  def most_visits
+    return @most_visits if @most_visits
+
+    visits = Hash.new(0)
+    log.each {|line| visits[line[0]] += 1 }
+
+    @most_visits = visits.sort_by {|path, visits| visits }.reverse
+  end
 end
--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -2,6 +2,9 @@ require 'rspec'
 require_relative '../lib/log_parser'
 
 describe LogParser do
+  let(:file_path) { 'spec/example.log' }
+  subject { described_class.new(file_path) }
+
   describe "#initialize" do
     context "with no arguments" do
       it "raises an exception" do
@@ -20,17 +23,33 @@ describe LogParser do
         let(:file_path) { '../test.xyz' }
 
         it "raises an exception" do
-          expect(->{ described_class.new(file_path) }).to raise_error Errno::ENOENT
+          expect(->{ subject }).to raise_error Errno::ENOENT
         end
       end
 
       context "where argument is a valid file path" do
-        let(:file_path) { 'webserver.log' }
-
         it "returns an instance" do
-          expect(described_class.new(file_path)).to be_a described_class
+          expect(subject).to be_a described_class
         end
       end
+    end
+  end
+
+  describe "#most_visits" do
+    it "returns an Array, each member consisting of request path and number of visits" do
+      expect(subject.most_visits).to be_a Array
+
+      subject.most_visits.each do |path|
+        expect(path[0]).to be_a String
+        expect(path[1]).to be_a Integer
+      end
+    end
+
+    it "orders the Hash according to most visits" do
+      expect(subject.most_visits.first[0]).to eq '/contact'
+      expect(subject.most_visits.first[1]).to eq 3
+      expect(subject.most_visits.last[0]).to eq '/index'
+      expect(subject.most_visits.last[1]).to eq 1
     end
   end
 end
